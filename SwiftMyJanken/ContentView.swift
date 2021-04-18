66b5c04632caadf1e358276a0ddb284120687bb0
koukikouhara <koki.kohara916@gmail.com>
2021-04-19 00:08:52 +0900

    Initial Commit

---
+++ SwiftMyJanken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftMyJanken
+//
+//  Created by 鴻原航輝 on 2021/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftMyJanken/SwiftMyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMyJankenApp.swift
+//  SwiftMyJanken
+//
+//  Created by 鴻原航輝 on 2021/04/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
